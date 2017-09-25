cd5c1d2cbf9c4ea431219bac34a12e47994e87a5
scorn <scorn@DESKTOP-JUOV8RK>
2017-09-25 23:11:33 +0200

    321

--- WPFUI/Views/ShellView.xaml.cs
+++ WPFUI/Views/ShellView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WPFUI.Views
 {
     /// <summary>
-    /// Interaction logic for ViewChild.xaml
+    /// Interaction logic for ShellView.xaml
     /// </summary>
-    public partial class ViewChild : UserControl
+    public partial class ShellView : Window
     {
-        public ViewChild()
+        public ShellView()
         {
             InitializeComponent();
         }
